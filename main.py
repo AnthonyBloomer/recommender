42c51632e1faa50a88f3e00caf8dc42c9ad5c073
Anthony Bloomer <ant0@protonmail.ch>
2018-12-30 01:55:26 +0000

    Update

---
+++ main.py
@@ -0,0 +1,16 @@
+from api import Recommender
+
+if __name__ == '__main__':
+    recommender = Recommender()
+    recommender.artists = 'Johnny Cash'
+    recommender.genres = [
+        'country',
+        'party'
+    ]
+    recommender.tunable_track_attributes = {
+        'danceability': 1.0
+    }
+
+    recommendations = recommender.find_recommendations()
+    for recommendation in recommendations['tracks']:
+        print("%s - %s" % (recommendation['album']['name'], recommendation['album']['artists'][0]['name']))
